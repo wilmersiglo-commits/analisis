b1d85ade184393e576cd97179595be4cc2e08b10
Wilmer <wilmersiglo@gmail.com>
2026-05-18 20:34:10 -0600

    lo arregle un poco

--- logina.js
+++ logina.js
@@ -2,8 +2,12 @@ let num1 = document.getElementById("num1")
 let num2 = document.getElementById("num2")
 let boton = document.getElementById("boton")
 let resultado
+let nombre=document.getElementById("nombre")
+let correo=document.getElementById("correo")
+let boton2=document.getElementById("boton2")
 
 boton.addEventListener("click",suma)
+boton2.addEventListener("click",mostrar)
 
 function suma (){
 
@@ -13,6 +17,22 @@ function suma (){
     resultado = n1+n2
 
     alert("el resultado es: " + resultado)
+
+}
+
+function mostrar (){
+
+    let n=nombre.value
+    let c=correo.value
+
+    if (n==' ' && c==' '){
+        alert("llene todos los campos")
+
+    }else{
+        alert("sus datos se agregaron correctamente")
+    }
+
+
 
 }
 
